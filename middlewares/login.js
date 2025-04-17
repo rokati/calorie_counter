7c76523a87538db66d225d4c24e4bc4170b43067
rokaati18 <rokaati18@edu.bme.hu>
2025-04-17 12:27:55 +0200

    feat: Improve login error handling and user feedback in the UI

--- middlewares/login.js
+++ middlewares/login.js
@@ -10,32 +10,32 @@ module.exports = (objRepo) => {
             return res.redirect('/calorie_counter');
         }
         UserModel.findOne({ username: req.body.username}).then(user => {
-            if (user) {
-                // Check if the password matches
-                if (user.password !== req.body.password) {
-                    return res.redirect('/'); // Redirect to login page if password is incorrect
-                }
-                req.session.loggedIn = true;
-                req.session.user = {
-                    username: user.username,
-                    goal_calories: user.goal_calories,
-                    consumed_calories: user.consumed_calories,
-                    consumed_carbs: user.consumed_carbs,
-                    consumed_proteins: user.consumed_proteins,
-                    consumed_fats: user.consumed_fats,
-                    meals: user.meals,
-                }
-                req.session.save(err => {
-                    if (err) {
-                        console.error('Error saving session:', err);
-                        return next(err); // Pass the error to the next middleware
-                    }
-                    return res.redirect('/calorie_counter');
-                });
-            } else {
-                console.log('User not found:', req.body.username);
-                return res.redirect('/');
+            if (!user) {
+                // User not found
+                return res.render('index', { error: 'User not found.' });
             }
+            if (user.password !== req.body.password) {
+                // Incorrect password
+                return res.render('index', { error: 'Password is not correct.' });
+            }
+            // Successful login
+            req.session.loggedIn = true;
+            req.session.user = {
+                username: user.username,
+                goal_calories: user.goal_calories,
+                consumed_calories: user.consumed_calories,
+                consumed_carbs: user.consumed_carbs,
+                consumed_proteins: user.consumed_proteins,
+                consumed_fats: user.consumed_fats,
+                meals: user.meals,
+            }
+            req.session.save(err => {
+                if (err) {
+                    console.error('Error saving session:', err);
+                    return next(err); // Pass the error to the next middleware
+                }
+                return res.redirect('/calorie_counter');
+            });
         }).catch(err => {
             console.error('Error during login:', err);
             return next(err);
